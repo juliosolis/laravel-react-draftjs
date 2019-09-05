49da437d970cec44762e0c233fe66c646bedf2f0
Julio César Solis Lainez <js@juliosolis.com>
2019-09-05 09:24:31 -0600

    UI upgrade - Add minimal changes on app

--- resources/views/email/edit.blade.php
+++ resources/views/email/edit.blade.php
@@ -1,29 +1,21 @@
-@extends('layouts.app')
+@extends('layouts.app2')
 
 @push('css')
     <link rel="stylesheet" href="{{ asset('css/rich-editor.css') }}">
 @endpush
 
 @section('content')
-    <div class="container-fluid">
-        <div class="row justify-content-center">
-            <div class="col-md-10">
-                <div class="card">
-                    <div class="card-header">
-                        Edit Email: "{{ $email->subject }}"
-                        <a href="{{ route('email') }}" class="btn btn-outline-info float-right">
-                            <i class="fa fa-arrow-left" aria-hidden="true"></i>
-                            Return
-                        </a>
-                    </div>
-
-                    <div class="card-body">
-
-                        <div id="editor" data-id="{{ $email->id }}"></div>
-
-                    </div>
-                </div>
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Edit Email: "{{ $email->subject }}"</h1>
+        <div class="btn-toolbar mb-2 mb-md-0">
+            <div class="btn-group mr-2">
+                <a href="{{ route('email') }}" class="btn btn-sm btn-outline-secondary">
+                    <i class="fa fa-arrow-left" aria-hidden="true"></i>
+                    Return
+                </a>
             </div>
         </div>
     </div>
+
+    <div id="editor" data-id="{{ $email->id }}"></div>
 @endsection
